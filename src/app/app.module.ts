16990a8131c3105e3dad89c5fcb098a33f7b62dd
s.de-pontfarcy <s.de-pontfarcy@groupeonepoint.com>
2018-04-20 11:38:35 +0200

    [init] project init with story component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { StoryComponent } from './story/story.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoryComponent
   ],
   imports: [
     BrowserModule
