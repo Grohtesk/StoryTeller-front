73bd880e40b797df932583029d1aeca813799df1
s.de-pontfarcy <s.de-pontfarcy@groupeonepoint.com>
2018-04-20 18:14:18 +0200

    [add] routing module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { StoryComponent } 		from './story/story.component';
+
+const routes: Routes = [
+	{ path: 'story', component: StoryComponent}
+];
+
+@NgModule({
+	imports: [RouterModule.forRoot(routes)],
+  	exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 
 import { AppComponent } from './app.component';
 import { StoryComponent } from './story/story.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 
 @NgModule({
@@ -12,7 +14,9 @@ import { StoryComponent } from './story/story.component';
     StoryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
